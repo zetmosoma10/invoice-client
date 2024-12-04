7a04ee5b1137eb0dfd87a04aae0b6ae6e64c2eb0
Zet <111925382+zetmosoma10@users.noreply.github.com>
2024-12-04 18:29:16 +0200

    Move API_URL to environment file

--- src/services/axiosInstance.js
+++ src/services/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://invoice-api-av4j.onrender.com/api",
+  baseURL: `${import.meta.env.VITE_API_URL}`,
 });
 
 axiosInstance.interceptors.request.use((config) => {
